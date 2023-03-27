fe39495910bef98f9e1edcaa325e52039ebdb305
Crossdefender <smartuwalaka@gmail.com>
2023-03-26 17:25:31 -0700

    stacks and queues

--- div.c
+++ div.c
@@ -1,40 +1,116 @@
 #include "monty.h"
-/**
- * f_div - divides the top two elements of the stack.
- * @head: stack head
- * @counter: line_number
- * Return: no return
-*/
-void f_div(stack_t **head, unsigned int counter)
-{
-	stack_t *h;
-	int len = 0, aux;
+#include "lists.h"
 
-	h = *head;
-	while (h)
+/**
+ * ivstg - investigate the operand to check if the input is alpha.
+ * @operand: the operand to investigate.
+ * @line_number: The line number of the opcode being worked on.
+ * Return: the operand
+ */
+char *ivstg(char *operand, unsigned int line_number)
+{
+	int i, k;
+
+	if (operand == NULL)
 	{
-		h = h->next;
-		len++;
+		erro(5, line_number);
 	}
-	if (len < 2)
+	i = 0;
+	k = (strlen(operand));
+	if (operand[0] == 45)
+		i++;
+	for (; i < k; i++)
 	{
-		fprintf(stderr, "L%d: can't div, stack too short\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
+		if (operand[i] > 47 && operand[i] < 58)
+			continue;
+		erro(5, line_number);
 	}
-	h = *head;
-	if (h->n == 0)
-	{
-		fprintf(stderr, "L%d: division by zero\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	aux = h->next->n / h->n;
-	h->next->n = aux;
-	*head = h->next;
-	free(h);
+	return (operand);
+}
+
+/**
+ * swapper - handles the swap instruction
+ * @stack: double pointer to the stack to push to
+ * @line_number: number of the line in the file
+ */
+void swapper(stack_t **stack, unsigned int line_number)
+{
+	stack_t *first = *stack, *second = NULL;
+
+	if (dlistint_len(*stack) < 2)
+		erro_ext(8, line_number);
+
+	second = first->next;
+
+	first->prev = second;
+	first->next = second->next;
+	second->next = first;
+	second->prev = NULL;
+	*stack = second;
+}
+
+/**
+ * adder - handles the add instruction
+ * @stack: double pointer to the stack to push to
+ * @line_number: number of the line in the file
+ */
+void adder(stack_t **stack, unsigned int line_number)
+{
+	stack_t *first = *stack, *second = NULL;
+	int sum = 0;
+
+	if (dlistint_len(*stack) < 2)
+		erro(9, line_number);
+
+	second = first->next;
+
+	sum = (first->n) + (second->n);
+	second->n = sum;
+	*stack = second;
+	free(first);
+}
+
+/**
+ * subber - handles the subtraction instruction
+ * @stack: double pointer to the stack to push to
+ * @line_number: number of the line in the file
+ */
+void subber(stack_t **stack, unsigned int line_number)
+{
+	stack_t *first = *stack, *second = NULL;
+	int diff = 0;
+
+	if (dlistint_len(*stack) < 2)
+		erro(10, line_number);
+
+	second = first->next;
+
+	diff = (second->n) - (first->n);
+	second->n = diff;
+	*stack = second;
+	free(first);
+}
+
+/**
+ * divider - handles the division instruction
+ * @stack: double pointer to the stack to push to
+ * @line_number: number of the line in the file
+ */
+void divider(stack_t **stack, unsigned int line_number)
+{
+	stack_t *start, *next;
+	int ans;
+
+	if (dlistint_len(*stack) < 2)
+		erro(11, line_number);
+	if ((*stack)->n == 0)
+		erro(12, line_number);
+
+	start = *stack;
+	next = start->next;
+	ans = (next->n) / (start->n);
+	*stack = next;
+	(*stack)->n = ans;
+	free(start);
+	(*stack)->prev = NULL;
 }
--- monty.h
+++ monty.h
@@ -1,12 +1,34 @@
 #ifndef MONTY_H
 #define MONTY_H
+
+#define _POSIX_C_SOURCE  200809L
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <fcntl.h>
 #include <string.h>
 #include <ctype.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+#define UNDEFINED 0
+#define DELIM " \r\t\n"
+#define USAGE "USAGE: monty file\n"
+#define NOACCESS "Error: Can't open file %s\n"
+#define INVALID "L%d: unknown instruction %s\n"
+#define MALLOC "Error: malloc failed\n"
+#define PUSH "L%d: usage: push integer\n"
+#define POP "L%d: can't pop an empty stack\n"
+#define PINT "L%d: can't pint, stack empty\n"
+#define SWAP "L%d: can't swap, stack too short\n"
+#define ADD "L%d: can't add, stack too short\n"
+#define SUB "L%d: can't sub, stack too short\n"
+#define DIV "L%d: can't div, stack too short\n"
+#define DERR "L%d: division by zero\n"
+#define MUL "L%d: can't mul, stack too short\n"
+#define MOD "L%d: can't mod, stack too short\n"
+#define PCHAR "L%d: can't pchar, stack empty\n"
+#define RANGE "L%d: can't pchar, value out of range\n"
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -22,57 +44,67 @@ typedef struct stack_s
 	struct stack_s *prev;
 	struct stack_s *next;
 } stack_t;
-/**
- * struct bus_s - variables -args, file, line content
- * @arg: value
- * @file: pointer to monty file
- * @content: line content
- * @lifi: flag change stack <-> queue
- * Description: carries values through the program
- */
-typedef struct bus_s
-{
-	char *arg;
-	FILE *file;
-	char *content;
-	int lifi;
-}  bus_t;
-extern bus_t bus;
+
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO Holberton project
+ * for stack, queues, LIFO, FIFO Alx project
  */
 typedef struct instruction_s
 {
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
-char *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);
-ssize_t getstdin(char **lineptr, int file);
-char  *clean_line(char *content);
-void f_push(stack_t **head, unsigned int number);
-void f_pall(stack_t **head, unsigned int number);
-void f_pint(stack_t **head, unsigned int number);
-int execute(char *content, stack_t **head, unsigned int counter, FILE *file);
-void free_stack(stack_t *head);
-void f_pop(stack_t **head, unsigned int counter);
-void f_swap(stack_t **head, unsigned int counter);
-void f_add(stack_t **head, unsigned int counter);
-void f_nop(stack_t **head, unsigned int counter);
-void f_sub(stack_t **head, unsigned int counter);
-void f_div(stack_t **head, unsigned int counter);
-void f_mul(stack_t **head, unsigned int counter);
-void f_mod(stack_t **head, unsigned int counter);
-void f_pchar(stack_t **head, unsigned int counter);
-void f_pstr(stack_t **head, unsigned int counter);
-void f_rotl(stack_t **head, unsigned int counter);
-void f_rotr(stack_t **head, __attribute__((unused)) unsigned int counter);
-void addnode(stack_t **head, int n);
-void addqueue(stack_t **head, int n);
-void f_queue(stack_t **head, unsigned int counter);
-void f_stack(stack_t **head, unsigned int counter);
+
+/**
+ * struct global_s - global struct variable
+ * @head: head of the stack
+ * @value: value passed into the opcode
+ * @line_number: line number of the opcode
+ * @bufline: line read from bytecode
+ * @fd: file descriptor
+ * @cmd: opcode
+ *
+ */
+typedef struct global_s
+{
+	stack_t *head;
+	char *value;
+	unsigned int line_number;
+	char *bufline;
+	FILE *fd;
+	char *cmd;
+	int mode;
+	unsigned int idx;
+} global_t;
+
+extern global_t var;
+
+void free_var(void);
+void free_stack(void);
+void run_cmd(char *bufline);
+void (*get_cmd(void))(stack_t **head, unsigned int line_number);
+void erro(int code, ...);
+void erro_ext(int code, int va_arg);
+char *ivstg(char *operand, unsigned int line_number);
+void pusher(stack_t **stack, unsigned int line_number);
+void paller(stack_t **stack, unsigned int line_number);
+void pinter(stack_t **stack, unsigned int line_number);
+void popper(stack_t **stack, unsigned int line_number);
+void swapper(stack_t **stack, unsigned int line_number);
+void adder(stack_t **stack, unsigned int line_number);
+void subber(stack_t **stack, unsigned int line_number);
+void divider(stack_t **stack, unsigned int line_number);
+void multpler(stack_t **stack, unsigned int line_number);
+void modder(stack_t **stack, unsigned int line_number);
+void nopper(stack_t **stack, unsigned int line_number);
+void pchar(stack_t **stack, unsigned int line_number);
+void rotl(stack_t **stack, unsigned int line_number);
+void rotr(stack_t **stack, unsigned int line_number);
+void pstr(stack_t **stack, unsigned int line_number);
+void changemode(stack_t **stack, unsigned int line_number);
+
 #endif
